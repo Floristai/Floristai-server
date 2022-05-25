9dca68a03ef1995422d4d54ab9806369146382a5
Aivaras Petrikas <aivaras.petrikas@dreamcubator.club>
2022-05-26 01:15:17 +0300

    new fields

--- Floristai/Entities/FlowerEntity.cs
+++ Floristai/Entities/FlowerEntity.cs
@@ -4,5 +4,11 @@
     {
         public int Id { get; set; }
         public string Name { get; set; }
+        public string ImageUrl { get; set; }
+        public float Price { get; set; }
+        public string Packaging { get; set; }
+        public string Occasion { get; set; }
+        public string Description { get; set; }
+        public int Quantity { get; set; }
     }
 }
